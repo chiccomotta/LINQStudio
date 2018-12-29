428d94d876b3296c66b620e117bd4f0e07b4efb9
cristiano motta <cristiano.motta@hotmail.it>
2018-12-29 22:44:57 +0100

    Add project files.

---
+++ LINQStudio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LINQStudio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
